8131490aed2f6f083d5d77535c3813b0f59005bf
Marina Ebbecke <m.ebbecke@gmail.com>
2025-10-15 20:08:53 -0300

    style(button): adds new variant and some style adjustments on Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { User } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import { Button } from './components';
 
 const activeTasks: number = 1; // Change number to test pluralization
 
@@ -9,6 +11,17 @@ const App = () => {
     <div>
       <h1>{t('helloWorld')}</h1>
       <p>{t('activeTasks', { count: activeTasks })}</p>
+      <div className="flex gap-2">
+        <Button startIcon={<User />}>Primary</Button>
+        <Button variant="secondary" startIcon={<User />}>
+          Secondary
+        </Button>
+        <Button variant="ghost" startIcon={<User />}>
+          Ghost
+        </Button>
+        <Button startIcon={<User />} size="icon" />
+        <Button variant="ghost" startIcon={<User />} size="icon" />
+      </div>
     </div>
   );
 };
--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,15 +1,17 @@
 import { tv } from 'tailwind-variants';
 
 const buttonVariants = tv({
-  base: 'flex items-center justify-center cursor-pointer text-white disabled:text-gray-400 rounded-lg py-2 px-4 gap-2 [&_svg]:size-5',
+  base: 'flex items-center justify-center h-10 cursor-pointer text-white disabled:text-gray-400 rounded-lg py-2 px-4 gap-2 [&_svg]:size-4.5',
   variants: {
     variant: {
       primary: 'bg-primary hover:bg-primary-hover disabled:bg-primary/50',
       secondary:
         'bg-transparent hover:bg-border border-1 border-border disabled:bg-transparent',
+      ghost:
+        'bg-transparent hover:bg-border disabled:bg-transparent disabled:text-gray-400',
     },
     size: {
-      icon: 'w-10 h-10',
+      icon: 'w-10 aspect-square',
       full: 'w-full',
     },
     disabled: {
